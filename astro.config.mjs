91813331a8264c8c77553316693c21e8d4d2ba4c
Young Sang Choi <16828483+youngsangchoi@users.noreply.github.com>
2026-09-03 00:03:23 -0400

    Update typography, dependencies, and mobile links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://reaim-lab.github.io",
+  compressHTML: true,
 
   build: {
     format: "file",
